631b2c12c134af9018656cfee6c361b99387e627
jarekj71 <jarek.jasiewicz@gmail.com>
2024-04-03 20:59:54 +0200

    zajecia 5

---
+++ geoml.py
@@ -0,0 +1,16 @@
+import geopandas as gpd
+import rasterio as rio
+
+class geoml:
+    def __init__(self):
+        pass
+
+    def _load_raster(self,raster=None):
+        pass
+
+    def _load_vector(self,vector=None):
+        pass
+
+    def _random_sample(self,n=0,p=0):
+        pass
+
